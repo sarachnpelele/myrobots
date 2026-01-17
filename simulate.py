0d4ab8f00ac3f73d1b718ac35be4f460a41595d0
Sara Chanaa <ss.sarachanaa@gmail.com>
2026-01-17 12:00:49 -0500

    Disabling the sidebars on the pybullet simulation

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
