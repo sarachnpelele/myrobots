bc3a09e3f59ee3f7b0e9e865e587dde064a03778
Sara Chanaa <ss.sarachanaa@gmail.com>
2026-02-02 13:41:45 -0500

    Fix world cube position so robot and world don't collide

--- generate.py
+++ generate.py
@@ -1,9 +1,24 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube( name="Block",pos=[0, 0, 0.5],size=[1, 1, 1] )
+def Create_World():
 
-pyrosim.End()
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube( name="Block",pos=[0, 5, 0.5],size=[1, 1, 1] )
+    pyrosim.End()
+
+
+
+def Create_Robot():
+
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube( name="Torso",pos=[0, 0, 0.5],size=[1, 1, 1] )
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
+
+
+
 
 """
 num_rows = 5       
