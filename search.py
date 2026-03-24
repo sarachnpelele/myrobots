e8eef3db5bfa11a1127eb099019483136c144d4f
Sara Chanaa <ss.sarachanaa@gmail.com>
2026-03-23 22:10:49 -0400

    created the hillclimber

--- search.py
+++ search.py
@@ -1,4 +1,8 @@
 import os
-for i in range(5):
+from hillclimber import HILL_CLIMBER
+
+"""for i in range(5):
     os.system("python3 generate.py")
     os.system("python3 simulate.py")
+"""
+hc = HILL_CLIMBER()
---
+++ solution.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class SOLUTION:
+    def __init__(self):
+        self.weights = np.random.rand(3, 2)
+        self.weights = self.weights * 2 - 1
+        
+        
