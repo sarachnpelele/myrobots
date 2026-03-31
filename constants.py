a7c3cd7d7df1ccff5684241f4e460f738e9abf20
Sara Chanaa <ss.sarachanaa@gmail.com>
2026-03-31 14:03:44 -0400

    parameterized the neural network

--- constants.py
+++ constants.py
@@ -16,6 +16,9 @@ ampFrontLeg = numpy.pi/4
 freqFrontLeg = 20
 phaseOffsetFrontLeg = 0
 
-numberOfGenerations = 10
+numberOfGenerations = 1
 
-populationSize = 10
+populationSize = 1
+
+numSensorNeurons = 3
+numMotorNeurons = 2
--- solution.py
+++ solution.py
@@ -3,12 +3,12 @@ import os
 import pyrosim.pyrosim as pyrosim
 import random as r
 import time
-
+import constants as c
 tester = True
 
 class SOLUTION:
     def __init__(self, nextAvailableID):
-        self.weights = np.random.rand(3, 2)
+        self.weights = np.random.rand(c.numSensorNeurons, c.numMotorNeurons)
         self.weights = self.weights * 2 - 1
         self.myID = nextAvailableID
         
@@ -60,19 +60,20 @@ class SOLUTION:
 
         pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
         pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
-        pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-        pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+        pyrosim.Send_Sensor_Neuron(name = c.numMotorNeurons, linkName = "FrontLeg")
+        pyrosim.Send_Motor_Neuron( name = c.numSensorNeurons , jointName = "Torso_BackLeg")
         pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_FrontLeg")
-        pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.5 )
-        pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -1.5)
+        pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = c.numSensorNeurons , weight = -1.5 )
+        pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = c.numSensorNeurons , weight = -1.5)
         pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = -1.5 )
         pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.5 )
 
-        for currentRow in range(3):
-            for currentColumn in range(2):
-                pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn+3, weight =  self.weights[currentRow][currentColumn] )
+        for currentRow in range(c.numSensorNeurons):
+            for currentColumn in range(c.numMotorNeurons):
+                pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn+c.numSensorNeurons, weight =  self.weights[currentRow][currentColumn] )
     
         pyrosim.End()
+        
 
     def Mutate(self):
         randomRow = r.randint(0,2)
