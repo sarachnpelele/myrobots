f54ddcf6546b55c0a538749d97793b56292d66d2
Sara Chanaa <ss.sarachanaa@gmail.com>
2026-03-31 14:06:09 -0400

    Broke body

--- solution.py
+++ solution.py
@@ -44,7 +44,7 @@ class SOLUTION:
 
         pyrosim.Start_URDF("body.urdf")
 
-        pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+        pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1], size=[1, 1, 1])
 
         pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
         pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
